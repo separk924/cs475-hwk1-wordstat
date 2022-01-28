737cacc2236378ba5d25b66a7649d6afd7aab3c1
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-01-28 02:28:49 -0500

    created menu and user input

--- menu.c
+++ menu.c
@@ -6,5 +6,9 @@
  */
 int getMenuOption()
 {
+	int resp = 0;
 	// TODO
+	printf("***WORD STATS MENU***\nEnter 1 to print vowel and consonant frequency.\nEnter 2 to print word count.\nEnter 3 to print histogram.\nEnter 4 to return to inputting more strings.\nEnter 5 to quit.\n");
+	scanf("%d", &resp);
+	return resp;
 }
