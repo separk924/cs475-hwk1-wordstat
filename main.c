3dbf7336d9db0cbe75bc5be297f7da58300f793c
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-01-28 02:32:00 -0500

    updated main() for user to input words and get stats of those words

--- main.c
+++ main.c
@@ -13,11 +13,65 @@
  */
 int main(int argc, char **argv)
 {
+	WordStats stats;
 	char input[MAX_INPUT_LEN]; //holds user-input string
-
+	char allInput[MAX_INPUT_LEN];
 	// TODO: repeatedly print menu options and prompt for an option
 	// TODO: display the proper output
+	printf("Enter strings (# to stop):\n");
+	fgets(input, MAX_INPUT_LEN, stdin);
+	strcat(allInput, input);
+	// while(input!='#'){
+	// 	fgets(input, MAX_INPUT_LEN, stdin);
+	// 	strcat(allInput, input);
+	// }
+	// int resp = getMenuOption();
+	// switch(getMenuOption()){
+	// 	case '1' :
+	// 		updateVowelCons(stats, allInput);
+	// 		printVowelConsFreq(stats);
+	// 		break;
+	// 	case '2' :
+	// 		printWordCount(stats);
+	// 		break;
+	// 	case '3' :
+	// 		printHistogram(stats, allInput);
+	// 		break;
+	// 	case '4' :
+	// 		printf("Enter strings (# to stop):\n");
+	// 		fgets(input, MAX_INPUT_LEN, stdin);
+	// 		strcat(allInput, input);
+	// 		while(input!='#'){
+	// 			fgets(input, MAX_INPUT_LEN, stdin);
+	// 			strcat(allInput, input);
+	// 		};
+	// 		break;
+	// 	case '5' :
+	// 		break;
+	// 	default :
+	// 	printf("Error: Unknown option %d. Try again.\n", input);
+	// }
 
+	do{
+		if(getMenuOption() == '1'){
+			updateVowelCons(stats, allInput);
+			printVowelConsFreq(stats);
+		}else if(getMenuOption() == '2'){
+			printWordCount(stats);
+		}else if(getMenuOption() == '3'){
+			printHistogram(stats, allInput);
+		}else if(getMenuOption() == '4'){
+			printf("Enter strings (# to stop):\n");
+			fgets(input, MAX_INPUT_LEN, stdin);
+			strcat(allInput, input);
+			while(input!='#'){
+				fgets(input, MAX_INPUT_LEN, stdin);
+				strcat(allInput, input);
+			};
+		}else{
+			printf("Error: Unknown option %d. Try again.\n", input);
+		}
+	}while(getMenuOption()=='5');
 	printf("Exiting...\n");
 	return 0;
 }
